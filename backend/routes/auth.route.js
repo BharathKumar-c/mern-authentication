ca09a3d98c7823d8e86e3f23c0a9f720b421909a
Bharath <83769872+BharathKumar-c@users.noreply.github.com>
2024-09-15 15:56:52 +0530

    complete check-auth with verifytoken middleware functionality

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,16 +6,20 @@ import {
   verifyEmail,
   forgetPassword,
   resetPassword,
+  checkAuth,
 } from '../controllers/auth.controller.js';
+import {verifyToken} from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
+router.get('/check-auth', verifyToken, checkAuth);
 router.post('/signup', signup);
 router.post('/Login', login);
 router.post('/logout', logout);
 
 router.post('/verify-email', verifyEmail);
 router.post('/forgot-password', forgetPassword);
+
 router.post('/reset-password/:token', resetPassword);
 
 export default router;
