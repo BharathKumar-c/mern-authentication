98dbe9007f3d8586df0a58e1d3e198d4e92a102c
Bharath <83769872+BharathKumar-c@users.noreply.github.com>
2024-09-15 16:28:57 +0530

    initial setup for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-rose-600 text-5xl">Mern - Authentication</h1>
+    </div>
+  );
+}
+
+export default App;
