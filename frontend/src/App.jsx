c3b9ab72027cd55b3c1ed440916e2bf41633c439
Bharath <83769872+BharathKumar-c@users.noreply.github.com>
2024-09-24 13:44:12 +0530

    signup page and related component is completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,38 @@
+import FloatingShape from './components/FloatingShape';
+import {Routes, Route} from 'react-router-dom';
+import {LoginPage, SignUpPage} from './pages/index';
+
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-rose-600 text-5xl">Mern - Authentication</h1>
+    <div
+      className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 
+    items-center justify-center flex relative overflow-hidden">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-40 h-40"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+      <Routes>
+        <Route path="/" element={'home'} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,71 @@
+import {motion} from 'framer-motion';
+import Input from '../components/Input';
+import {useState} from 'react';
+import {User, Mail, Lock} from 'lucide-react';
+import {Link} from 'react-router-dom';
+import PasswordStrengthMeter from '../components/PasswordStrengthMeter';
+
+const SignUpPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <motion.div
+      initial={{opacity: 0, y: 20}}
+      animate={{opacity: 1, y: 0}}
+      transition={{duration: 0.5}}
+      className="w-full max-w-md overflow-hidden bg-gray-800 bg-opacity-50 shadow-xl backdrop-filter backdrop-blur-xl rounded-2xl">
+      <div className="p-[2.5rem]">
+        <h1 className="mb-6 text-2xl font-bold text-center text-transparent bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text">
+          Create Account
+        </h1>
+        <form onSubmit={handleSignup}>
+          <Input
+            icon={User}
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Full Name"
+          />
+          <Input
+            icon={Mail}
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email Address"
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          {/* Password strength meter */}
+          <PasswordStrengthMeter password={password} />
+
+          <motion.button
+            className="w-full px-4 py-3 mt-5 font-bold text-white transition duration-200 rounded-lg shadow-lg bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 foucs:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+            type="submit"
+            whileHover={{scale: 1.02}}
+            whileTap={{scale: 0.98}}>
+            Sign Up
+          </motion.button>
+        </form>
+      </div>
+      <div className="flex justify-center px-8 py-4 bg-gray-900 bg-opacity-50">
+        <p className="text-sm text-gray-400">
+          Already have an account?{' '}
+          <Link to={'/login'} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default SignUpPage;
